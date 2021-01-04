18353e5bc6ae1a3756a15b3290888c0c48bb2713
bwd136 <60761353+bwd136@users.noreply.github.com>
2021-01-04 15:06:25 -0500

    Testing refresh 1

---
+++ js/slidershow.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $('.slideshow').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1082,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+          
+              
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+
+      $('.slideshow').slickLightbox({
+        src: 'src',
+        itemSelector: 'img',
+        navigateByKeyboard: true,
+        background: 'rgba(251,250,248)'
+      });
+  });
