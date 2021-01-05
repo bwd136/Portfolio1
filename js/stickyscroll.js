ca735f473bee3eda754fc09c66a5401e337771a2
bwd136 <60761353+bwd136@users.noreply.github.com>
2021-01-05 13:07:05 -0500

    refresh testing 5

--- js/stickyscroll.js
+++ js/stickyscroll.js
@@ -1,13 +1,13 @@
 $(document).ready(function() { 
+    $('.herobox').parallaxScroll({
+        friction: 1
+    });
     $('.header-container').scrollToFixed({ 
         marginTop: 0, limit: 1800});
 });
 
 $(document).ready(function () {
     $(window).scroll(function () {
-        $(".herobox").parallaxie({
-            "pos_x": '70%'
-        });
         $(".herobox").css("opacity", 1 - $(window).scrollTop() / $('.herobox').height()*6);
     
         // if ($(document).scrollTop() > 1225) {
